b2d43f390a619e411b149b791bc058d36742198a
Jim Laney <jlaney@uw.edu>
2020-10-07 08:47:12 -0700

    overrides close

--- memcached_clients/django.py
+++ memcached_clients/django.py
@@ -8,7 +8,6 @@ class PymemcacheCache(BaseMemcachedCache):
     Implementation of a pymemcache binding for Django.
     """
     def __init__(self, server, params):
-        self._local = local()
         super().__init__(server, params, library=pymemcache,
                          value_not_found_exception=KeyError)
         self._class = self._lib.HashClient
@@ -17,3 +16,9 @@ class PymemcacheCache(BaseMemcachedCache):
             "default_noreply": False,
             "serde": serde.pickle_serde,
         })
+
+    def close(self, **kwargs):
+        # Don't call disconnect_all() if connection pooling is enabled,
+        # as it resets the failover state and creates unnecessary reconnects.
+        if not self._cache.use_pooling:
+            self._cache.disconnect_all()
