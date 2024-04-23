1cb6dda3c4e046614d28d66f77ad5509ec4122ea
Jim Laney <jlaney@uw.edu>
2024-04-23 12:04:47 -0700

    drop import

--- memcached_clients/restclient.py
+++ memcached_clients/restclient.py
@@ -1,7 +1,7 @@
 # Copyright 2024 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-from memcached_clients import PymemcacheClient, MemcacheError
+from memcached_clients import PymemcacheClient
 from commonconf import settings
 from hashlib import sha1
 from logging import getLogger
